39ad186e0b3784ccd3186d53e4ab7ca24e5e9da1
Brian Juma <bjuma0985@gmail.com>
2024-05-23 17:40:29 +0300

    looping

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+  squared_list = [x ** 2 for x in int_list]
+  return squared_list
+int_list = [2, 4, 6, 8, 10]
+squared_list = square_integers(int_list)
+print(squared_list)
+  
 
 def fizzbuzz():
-    # code goes here!
-    pass
+  for i in range(1, 101):
+      if i % 3 == 0 and i % 5 == 0:
+          print("FizzBuzz")
+      elif i % 3 == 0:
+          print("Fizz")
+      elif i % 5 == 0:
+          print("Buzz")
+      else:
+          print(i)
+
+fizzbuzz()
